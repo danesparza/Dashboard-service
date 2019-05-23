36cea5424a945860a64ed586ee810ef50fb322ba
Dan Esparza <esparza.dan@gmail.com>
2019-05-23 15:00:26 -0400

    Add unit tests for config

--- data/config_test.go
+++ data/config_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/danesparza/Dashboard-service/data"
 )
 
-func TestConfig_AddConfig_ValidConfig_Successful(t *testing.T) {
+func TestConfig_SetConfig_ValidConfig_Successful(t *testing.T) {
 
 	//	Arrange
 	systemdb := getTestFiles()
@@ -29,5 +29,35 @@ func TestConfig_AddConfig_ValidConfig_Successful(t *testing.T) {
 	}
 
 	t.Logf("Set config: %+v", response)
+
+}
+
+func TestConfig_GetConfig_ValidConfig_Successful(t *testing.T) {
+
+	//	Arrange
+	systemdb := getTestFiles()
+	db, err := data.NewManager(systemdb)
+	if err != nil {
+		t.Errorf("NewManager failed: %s", err)
+	}
+	defer func() {
+		db.Close()
+		os.RemoveAll(systemdb)
+	}()
+
+	//	Act
+	_, err = db.SetConfig("Name1", "Value1")
+	if err != nil {
+		t.Errorf("SetConfig - Should execute without error, but got: %s", err)
+	}
+
+	response, err := db.GetConfig("Name1")
+
+	//	Assert
+	if err != nil {
+		t.Errorf("SetConfig - Should execute without error, but got: %s", err)
+	}
+
+	t.Logf("Get config: %+v", response)
 
 }
